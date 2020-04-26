8a67b9d0116214d1d8c4e7c1ff579753a37ad46e
Xiaoyi Liu <lxy878@gmail.com>
2020-04-26 10:20:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,8 +37,9 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/articles/:id' do
-    @articles = Article.find(params[:id])
-    
+    @article = Article.find(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
     redirect ''
   end
 end
