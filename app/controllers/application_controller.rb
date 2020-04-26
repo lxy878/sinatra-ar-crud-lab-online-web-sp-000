1821c11ebb7aabc9243923ec0f4ad72b0622f2c8
Xiaoyi Liu <lxy878@gmail.com>
2020-04-26 10:42:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,6 @@ class ApplicationController < Sinatra::Base
 
   delete '/articles/:id' do
     # @article = Article.find(params[:id]).destory
-    # redirect '/articles'
-    'delete'
+    redirect '/articles'
   end
 end
